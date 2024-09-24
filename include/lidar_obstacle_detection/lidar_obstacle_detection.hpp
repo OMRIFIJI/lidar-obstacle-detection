b389ff5a12217bea0ab576d94a70ba7641303d41
OMRIFIJI <amirkarimov46@gmail.com>
2024-09-24 14:10:31 +0300

    Input

--- include/lidar_obstacle_detection/lidar_obstacle_detection.hpp
+++ include/lidar_obstacle_detection/lidar_obstacle_detection.hpp
@@ -2,11 +2,23 @@
 #define LIDAR_OBSTACLE_DETECTION_HPP
 
 #include <ros/ros.h>
-#include <std_msgs/String.h>
+
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl_ros/point_cloud.h>
+
+#include <sensor_msgs/PointCloud2.h>
+
+typedef pcl::PointXYZ PointT;
 
 class ObstacleDetectionNode {
   public:
-    ObstacleDetectionNode(int argc, char **argv);
+	ObstacleDetectionNode(int argc, char **argv);
+  
+  private:
+    ros::Publisher _obstacle_pub;
+    ros::Subscriber _lidar_sub;
+    void _obstacle_detection_callback(const pcl::PointCloud<PointT>& src_cloud);
 };
 
 #endif
--- src/lidar_obstacle_detection/lidar_obstacle_detection.cpp
+++ src/lidar_obstacle_detection/lidar_obstacle_detection.cpp
@@ -1,9 +1,17 @@
 #include <lidar_obstacle_detection/lidar_obstacle_detection.hpp>
+#include <memory>
 
 ObstacleDetectionNode::ObstacleDetectionNode(int argc, char **argv) {
 	ros::init(argc, argv, "lidar_obstacle_detection");
 	ros::NodeHandle node_handle;
-	ros::Publisher chatter_pub =
-		node_handle.advertise<std_msgs::String>("obstacle_pub", 1000);
+	_obstacle_pub = node_handle.advertise<sensor_msgs::PointCloud2>("obstacle_pub", 10);
+	_lidar_sub = node_handle.subscribe(
+		"velodyne_points", 10,
+		&ObstacleDetectionNode::_obstacle_detection_callback, this);
 	ros::spin();
+}
+
+
+void ObstacleDetectionNode::_obstacle_detection_callback(const pcl::PointCloud<PointT>& src_cloud){
+    
 }
