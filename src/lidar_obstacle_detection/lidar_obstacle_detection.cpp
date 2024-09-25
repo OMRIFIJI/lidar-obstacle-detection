7990935da166209a9cd69d988f9c2de8e4817e79
OMRIFIJI <amirkarimov46@gmail.com>
2024-09-26 01:29:05 +0300

    param fix

--- src/lidar_obstacle_detection/lidar_obstacle_detection.cpp
+++ src/lidar_obstacle_detection/lidar_obstacle_detection.cpp
@@ -12,8 +12,8 @@ ObstacleDetectionNode::ObstacleDetectionNode(ros::NodeHandle node_handle, float 
 	_obstacle_points->header.frame_id = _lidar_frame_id;
 	_dangerous_obstacle_points->header.frame_id = _lidar_frame_id;
 
-	node_handle.param<float>("obstacle_sector_angle", _obstacle_sector_angle, 0.523598);
-	node_handle.param<float>("obstacle_sector_radius", _obstacle_sector_radius, 9);
+	node_handle.param<float>("/lidar_obstacle_detection_node/obstacle_sector_angle", _obstacle_sector_angle, 0.523598);
+    node_handle.param<float>("/lidar_obstacle_detection_node/obstacle_sector_radius", _obstacle_sector_radius, 9);
 	std::cout << "Param angle thrsh:" << angle_thrsh << std::endl;
 	std::cout << "Param sector angle:" << _obstacle_sector_angle << std::endl;
 	std::cout << "Param sector radius:" << _obstacle_sector_radius << std::endl;
